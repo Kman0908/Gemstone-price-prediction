b34d901a74a47319d232b3c6b5da77032b72221f
Krish Sharma <krish9pro@gmail.com>
2026-02-24 15:52:20 +0530

    last touch

--- app/frontend.py
+++ app/frontend.py
@@ -3,6 +3,8 @@ import requests
 from src.gemstones.exception import CustomException
 from src.gemstones.logger import logging
 import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 st.title('Gemstones Price Predictor')
 
